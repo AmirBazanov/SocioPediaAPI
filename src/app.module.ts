9d38713521bc9df33cd90536528b8aeecb81f548
AmirBazanov <flash5651311@gmail.com>
2023-10-14 10:55:02 +0500

    Added and init prisma and added resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ChatModule } from './chat/chat.module';
+import { PostsModule } from './posts/posts.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, PostsModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
